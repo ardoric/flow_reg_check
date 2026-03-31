33de300eb9f3be07d8186c27f4914b5b51fe0046
ardoRic <ardoric@gmail.com>
2026-03-31 16:06:14 +0000

    add simple license file checker.

---
+++ check_licenses.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+
+with open('licenses_link.json') as f:
+    prev = json.load(f)
+
+r         = requests.get('https://sites.google.com/view/agilitycpcportugal/p%C3%A1gina-inicial')
+soup      = BeautifulSoup(r.text,features="lxml")
+curr_link = soup.find_all(attrs={'aria-label':'Licenças'})[1]['href']
+
+if prev['link'] != curr_link:
+    print("AVISO: Novo ficheiro de licencas!!")
+    print(curr_link)
+
