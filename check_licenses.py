39f962c3c9d90908e75dfdc5b0609abed248526c
ardoRic <ardoric@gmail.com>
2026-04-10 13:57:57 +0000

    download license csv when detected

--- check_licenses.py
+++ check_licenses.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+from urllib.parse import urlsplit,urlunsplit
+import datetime
+
+
+def download(url, file):
+    with open(file, 'wb') as f:
+        r = requests.get(url)
+        f.write( r.content )
+
+def convert(url):
+    u = urlsplit(url)
+    u = u._replace(query='format=csv')._replace(path=u.path.replace('/edit','/export'))
+    return urlunsplit(u)
 
 
 with open('licenses_link.json') as f:
@@ -14,4 +27,11 @@ if prev['link'] != curr_link:
     print("AVISO: Novo ficheiro de licencas!!")
     print()
     print(curr_link)
+    
+    download(convert(curr_link), f'licenses-{datetime.date.today()}.csv')
+    with open('licenses_link.json','w') as f:
+        prev['link'] = curr_link
+        json.dump(prev, f, indent=2)
+
+
 
