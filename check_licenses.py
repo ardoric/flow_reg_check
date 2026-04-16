0a9e0a6114247b477fc6c4c38b91deb27746db88
ardoRic <ardoric@gmail.com>
2026-04-16 21:02:27 +0000

    also check for license file changes, hopefully

--- check_licenses.py
+++ check_licenses.py
@@ -3,12 +3,20 @@ import requests
 import json
 from urllib.parse import urlsplit,urlunsplit
 import datetime
+import hashlib
 
 
 def download(url, file):
+    digest = ''
     with open(file, 'wb') as f:
         r = requests.get(url)
         f.write( r.content )
+        digest = hashlib.sha256(r.content).hexdigest()
+    return digest
+
+def digest_url(url):
+    return hashlib.sha256(requests.get(url).content).hexdigest()
+
 
 def convert(url):
     u = urlsplit(url)
@@ -23,14 +31,15 @@ r         = requests.get('https://sites.google.com/view/agilitycpcportugal/p%C3%
 soup      = BeautifulSoup(r.text,features="lxml")
 curr_link = soup.find_all(attrs={'aria-label':'Licenças'})[1]['href']
 
-if prev['link'] != curr_link:
+if prev['link'] != curr_link or digest_url(convert(curr_link)) != prev['digest']:
     print("AVISO: Novo ficheiro de licencas!!")
     print()
     print(curr_link)
     
-    download(convert(curr_link), f'licenses-{datetime.date.today()}.csv')
+    digest = download(convert(curr_link), f'licenses-{datetime.date.today()}.csv')
     with open('licenses_link.json','w') as f:
         prev['link'] = curr_link
+        prev['digest'] = digest
         json.dump(prev, f, indent=2)
 
 
